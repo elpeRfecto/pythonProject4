e6f5a057655a44629741e6897dd5a8d101451980
elpeRfecto <mmjkee@yandex.ru>
2022-03-22 20:28:34 +0300

    ЗАДАНИЕ 13.8.19

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,9 +1,4 @@
-money = int(input("Введите сумму, которую хотите положить:"))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-for deposit in per_cent.values():
-    result = int(money / 100 * per_cent)
-    deposit.append(result)
-print(deposit)
-print("Максимальная сумма, которая накопится за год", "-", max(deposit))
-
+a = 179 ** 10
+b = 181 ** 9
+c = a + b
+print(c)
